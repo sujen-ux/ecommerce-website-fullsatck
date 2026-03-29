d4f60f43f036c9afab6dbbf1fcc32d9660556131
unknown <sujenbogati10@gmail.com>
2026-03-29 19:05:44 +0545

    Updated the code and Connected MongoDB in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
@@ -6,8 +7,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// MongoDB connection
+mongoose.connect("mongodb://mongo:27017/ecommerce")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
-  res.send("E-commerce backend is running...");
+  res.send("E-commerce backend running with MongoDB...");
 });
 
 app.listen(5000, () => {
